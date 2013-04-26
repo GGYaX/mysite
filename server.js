93d738debf0639d77b51b1c0a6550093cebe30b5
Yanxin Gong <yanxin.gong22@gmail.com>
2013-04-27 01:49:44 +0200

    add timelinejs and .gitignore

--- server.js
+++ server.js
@@ -12,13 +12,23 @@ http.createServer(function (request, response) {
 		filePath = './index.html';
 		 
 	var extname = path.extname(filePath);
-	var contentType = 'text/html';
+	console.log('extname is: >>> : ' + extname);
+	var contentType = 'text/plain';
 	switch (extname) {
+		case '.html':
+			contentType = 'text/html';
+			break;
 		case '.js':
 			contentType = 'text/javascript';
 			break;
 		case '.css':
 			contentType = 'text/css';
+			break;
+		case '.png':
+			contentType = 'image/png';
+			break;
+		case '.jpeg', 'jpg':
+			contentType = 'image/jpeg';
 			break;
 	}
 	 
